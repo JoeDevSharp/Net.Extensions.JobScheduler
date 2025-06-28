96e7fa993ef475135815f9d3c2018bac128b5f2a
José Miguel <j.ramos@jm-ramos.com>
2025-06-28 14:58:15 +0200

    Add RecurringJob implementation
    Introduces the RecurringJob class implementing IJob, allowing execution of a provided asynchronous action with cancellation support.

---
+++ Net.Extensions.JobScheduler/Jobs/RecurringJob.cs
@@ -0,0 +1,25 @@
+﻿using Net.Extensions.JobScheduler.Abstractions;
+using Net.Extensions.JobScheduler;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Net.Extensions.JobScheduler.Jobs
+{
+    /// <summary>
+    /// Job recurrente que ejecuta una tarea específica.
+    /// </summary>
+    public class RecurringJob : IJob
+    {
+        private readonly Func<CancellationToken, Task> _action;
+
+        public RecurringJob(Func<CancellationToken, Task> action)
+        {
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+        }
+
+        public Task ExecuteAsync(JobContext context, CancellationToken cancellationToken = default)
+        {
+            return _action(cancellationToken);
+        }
+    }
+}
